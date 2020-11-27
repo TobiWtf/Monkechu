999d7315f2dcd3fcc547d92f9134adc4b3b83d02
Tobi <=>
2020-11-27 16:55:04 -0600

    Windows build tools added

---
+++ windows-packager.js
@@ -0,0 +1,26 @@
+const electronInstaller = require('electron-winstaller');
+
+const runInstaller = async () => {
+    try {
+        await electronInstaller.createWindowsInstaller({
+                appDirectory: 'builds/windows/iMonke',
+                outputDirectory: 'builds/WindowsFull/',
+                authors: "Tobi, MakeShiftArtist",
+                exe: 'iMonke.exe',
+                owners: "Tobi, MakeShiftArtist, Zero",
+                description: "imagine if ifunny was good?",
+                version: "1.2.7",
+                title: "iMonke",
+                name: "iMonke",
+                setupIcon: "src/monkeshare/dump/imonke-new-leaning-padded-large.ico",
+                setupExe: "iMonke.exe",
+                setupMsi: "iMonke.msi"
+            },
+        );
+        console.log('It worked!');
+    } catch (e) {
+        console.log(`No dice: ${e.message}`);
+    };
+};
+
+runInstaller();
